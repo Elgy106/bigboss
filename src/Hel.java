089e8655b0375994fba7b0118fc70b0c65d30c43
Elgy106 <1064959837@qq.com>
2019-06-01 14:41:53 +0800

    led the way www

--- src/Hel.java
+++ src/Hel.java
@@ -1,5 +1,6 @@
 public class Hel {
     public static void main(String[] args) {
-        System.out.println(" the key!");
+        System.out.println(" the key is calm please be mine!");
+
     }
 }
